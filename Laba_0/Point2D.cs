7aeaaca1ff47ad9a6399495d4b69766988bca015
Соня Курочкина <duro4kinasonya@gmail.com>
2026-09-18 00:20:09 +0700

    Реализация класса Point2D

---
+++ Laba_0/Point2D.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ShapesDrawing
+{
+    public class Point2D
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public Point2D(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void AddX(int x)
+        {
+            X += x;
+        }
+
+        public void AddY(int y)
+        {
+            Y += y;
+        }
+    }
+}
