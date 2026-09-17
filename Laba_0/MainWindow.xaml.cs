65fad0f7a5e13787242c9c1cd619fa1b67ba8efa
Соня Курочкина <duro4kinasonya@gmail.com>
2026-09-18 00:43:20 +0700

    Отрисовка треугольника со случайными координатами и очистка сцены

--- Laba_0/MainWindow.xaml.cs
+++ Laba_0/MainWindow.xaml.cs
@@ -1,28 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Laba_0
+namespace ShapesDrawing
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly List<IShape> shapes = new List<IShape>();
+        private readonly Random rnd = new Random();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private int RandomX() => rnd.Next(0, (int)Scene.Width);
+        private int RandomY() => rnd.Next(0, (int)Scene.Height);
+
+        private void RedrawAll()
+        {
+            Scene.Children.Clear();
+            foreach (var shape in shapes)
+            {
+                shape.Draw(Scene);
+            }
+        }
+
+        private void BtnRandomTriangle_Click(object sender, RoutedEventArgs e)
+        {
+            Point2D p1 = new Point2D(RandomX(), RandomY());
+            Point2D p2 = new Point2D(RandomX(), RandomY());
+            Point2D p3 = new Point2D(RandomX(), RandomY());
+            shapes.Add(new Triangle(p1, p2, p3));
+            RedrawAll();
+        }
+
+        private void BtnClear_Click(object sender, RoutedEventArgs e)
+        {
+            shapes.Clear();
+            Scene.Children.Clear();
         }
     }
 }
